a18c35d66feca7ebb9396599d3000c048f3873c8
DougIT <doug@proeditors.com>
2017-05-26 23:05:53 -0600

    List and Elements, AddToFront and AddToBack

---
+++ ListFlattening/ListFlattening/LinkedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListFlattening {
+  public class LinkedList<T> {
+    private ListElement<T> head;
+    private ListElement<T> tail;
+
+    public LinkedList() {}
+
+    public void AddToFront(T data) {
+      ListElement<T> newElement = new ListElement<T>(data);
+      if(head == null) { //First element
+        head = newElement;
+        tail = newElement;
+      } else {
+        newElement.Next = head;
+        head.Previous = newElement;
+        head = newElement;
+      }
+    }
+
+    public void AddToBack(T data) {
+      ListElement<T> newElement = new ListElement<T>(data);
+      if(tail == null) {
+        tail = newElement;
+        head = newElement;
+      } else {
+        newElement.Previous = tail;
+        tail.Next = newElement;
+        tail = newElement;
+      }
+    }
+  }
+}
---
+++ ListFlattening/ListFlattening/ListElement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListFlattening {
+  public class ListElement<T> {
+    public T Data { get; private set; }
+    public ListElement<T> Previous { get; set; }
+    public ListElement<T> Next { get; set; }
+    public LinkedList Child { get; set; }
+    
+    public ListElement(T data) {
+      Data = data;
+    }
+  }
+}
