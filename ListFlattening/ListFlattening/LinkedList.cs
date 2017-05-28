bf60d58befb826502906195471bfa00491bcc5d9
DougIT <doug@proeditors.com>
2017-05-27 21:43:49 -0600

    Added indexer started setting up List Flattening problem data

--- ListFlattening/ListFlattening/LinkedList.cs
+++ ListFlattening/ListFlattening/LinkedList.cs
@@ -8,8 +8,23 @@ namespace ListFlattening {
   public class LinkedList<T> {
     private ListElement<T> head;
     private ListElement<T> tail;
-
+    public int Count { get; set; }
     public LinkedList() {}
+
+    public ListElement<T> this[int index] {
+      get {
+        //not efficient as we could decide whether or not its a shorter distance from
+        //the head or tail to the element at the target index
+        if (index < 0 || index >= Count) {
+          throw new IndexOutOfRangeException();
+        }
+        ListElement<T> currentElement = head;
+        for (int i = 0; i < index; i++) {
+          currentElement = currentElement.Next;
+        }
+        return currentElement;
+      }
+    }
 
     public void AddToFront(T data) {
       ListElement<T> newElement = new ListElement<T>(data);
@@ -21,6 +36,7 @@ namespace ListFlattening {
         head.Previous = newElement;
         head = newElement;
       }
+      Count++;
     }
 
     public void AddToBack(T data) {
@@ -33,6 +49,7 @@ namespace ListFlattening {
         tail.Next = newElement;
         tail = newElement;
       }
+      Count++;
     }
   }
 }
--- ListFlattening/ListFlattening/Program.cs
+++ ListFlattening/ListFlattening/Program.cs
@@ -7,6 +7,23 @@ using System.Threading.Tasks;
 namespace ListFlattening {
   class Program {
     static void Main(string[] args) {
+      LinkedList<int> list1 = new LinkedList<int>();
+      list1.AddToBack(5);
+      list1.AddToBack(33);
+      list1.AddToBack(17);
+      list1.AddToBack(2);
+      list1.AddToBack(1);
+      for(int i = 0; i < list1.Count; i++) {
+        Console.WriteLine(list1[i].Data);
+      }
+      LinkedList<int> list2 = new LinkedList<int>();
+      LinkedList<int> list3 = new LinkedList<int>();
+      LinkedList<int> list4 = new LinkedList<int>();
+      LinkedList<int> list5 = new LinkedList<int>();
+      LinkedList<int> list6 = new LinkedList<int>();
+      LinkedList<int> list7 = new LinkedList<int>();
+      LinkedList<int> list8 = new LinkedList<int>();
+      Console.ReadLine();
     }
   }
 }
